82f33d7931c7a4153eac8ce9731273cee96f7bc3
vaisakh <vaisakhkm2625@gmail.com>
2023-01-02 20:39:19 +0530

    hello world

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write,BufReader,BufRead,ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("What is your name?");
+
+    let mut name: String = String::new();
+    let greeting: &str = "Nice to meet you";
+
+    io::stdin().read_line(&mut name)
+        .expect("Didn't recevice input");
+
+    println!("Hello, {}! {}",name.trim_end(),greeting);
+}
